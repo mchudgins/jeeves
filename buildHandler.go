a033247bb5c4d792c8892cb33076ba3b720aab9f
Mike Hudgins <mchudgins@gmail.com>
2016-04-15 23:48:16 -0500

    parsing of /builds

--- buildHandler.go
+++ buildHandler.go
@@ -6,14 +6,41 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"regexp"
 	"strings"
 )
 
+var buildRegex *regexp.Regexp
+
+func init() {
+	log.Printf("init called.")
+	buildRegex = regexp.MustCompile("/builds/(?P<image>[a-zA-Z:]*)/(?P<buildName>[[:alpha:]]*)(?P<excess>/.*){0,}")
+}
+
 func BuildHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("processing htp request: %v\n", *r)
+	log.Printf("processing http request: %v\n", *r)
+
+	url := r.URL.RequestURI()
+	log.Printf("URL:  %s\n", url)
+	log.Printf("Query: %v", r.URL.Query())
+	log.Printf("size: %d", len(buildRegex.FindStringSubmatch(url)))
+	for _, s := range buildRegex.FindStringSubmatch(url) {
+		log.Printf("element: %s", s)
+	}
+	elements := buildRegex.FindStringSubmatch(url)
+	if len(elements) != 4 {
+		http.Error(w, http.StatusText(404), 404)
+		return
+	}
+
+	imageName := elements[1]
+	buildName := elements[2]
+
+	log.Printf("imageName: %s; buildName: %s", imageName, buildName)
 
 	switch strings.ToUpper(r.Method) {
 	case "POST":
+
 		if r.Body != nil {
 			body, err := ioutil.ReadAll(r.Body)
 			if err != nil {
@@ -26,6 +53,8 @@ func BuildHandler(w http.ResponseWriter, r *http.Request) {
 				log.Printf("read: %s\n\n", body)
 			}
 		}
+
+		http.Error(w, http.StatusText(400), 400)
 
 	default:
 		fmt.Fprintf(w, "%s : %q", r.Method, html.EscapeString(r.URL.Path))
--- buildHandler_test.go
+++ buildHandler_test.go
@@ -15,7 +15,12 @@ func TestPost(t *testing.T) {
 		expect int
 	}{
 		{url: "/builds/golang:latest", body: "", expect: 400},
+		{url: "/builds/golang:latest/fubar", body: "", expect: 400},
+		{url: "/builds/golang:latest/fubar/gorf", body: "", expect: 400},
+		{url: "/builds/golang:latest/fubar?gorf", body: "", expect: 400},
 	}
+
+	var failed bool
 
 	for _, c := range cases {
 		var reader io.Reader
@@ -23,20 +28,25 @@ func TestPost(t *testing.T) {
 			reader = strings.NewReader(c.body)
 		}
 
-		req, err := http.NewRequest("POST", "http://localhost/"+c.url, reader)
+		req, err := http.NewRequest("POST", "http://localhost"+c.url, reader)
 		if err != nil {
 			t.Fatal(err)
 		}
 
 		req.Header.Set("Content-Type", "application/json")
-		req.Header.Set("Accept-Encoding", "gzip")
+		req.Header.Set("Accept-Encoding", "gzip, deflate, sdch")
 
 		w := httptest.NewRecorder()
 
 		BuildHandler(w, req)
 
 		if w.Code != c.expect {
-			t.Fatalf("Expected HTTP Status Code of %d.  Got %d.  URL = %s\n", c.expect, w.Code, c.url)
+			t.Logf("Expected HTTP Status Code of %d.  Got %d.  URL = %s\n", c.expect, w.Code, c.url)
+			failed = true
+		}
+
+		if failed {
+			t.Fail()
 		}
 	}
 
